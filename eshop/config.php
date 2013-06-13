8ef7485759e25cfae083315d235e0e1831194876
janstoffer <janstoffer@seznam.cz>
2013-06-13 17:20:55 +0200

    eshop vamcart.com

---
+++ eshop/config.php
@@ -0,0 +1,23 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   VamCart - http://vamcart.com
+   -----------------------------------------------------------------------------------------
+   Copyright (c) 2011 VamSoft Ltd.
+   License - http://vamcart.com/license.html
+   ---------------------------------------------------------------------------------------*/
+
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'honza',
+		'password' => 'test',
+		'database' => 'eshop',
+		'prefix' => '',
+		'encoding' => 'utf8' 
+	);
+
+}
+?>
